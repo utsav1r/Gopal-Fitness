6372a08de5a517da19891c10707ebef2745915a9
utsav1r <utsavgopalpandey90148@gmail.com>
2025-04-30 22:22:02 +0530

    Add files via upload

---
+++ first.js
@@ -0,0 +1,8 @@
+
+document.getElementById('signupBtn').addEventListener('click', function() {
+    alert('Thank you for your interest! Please scroll down to sign up.');
+});
+
+document.getElementById('joinNowBtn').addEventListener('click', function() {
+    alert('Welcome to Skyline Fitness! Check your email for next steps.');
+});
